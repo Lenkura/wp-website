6ac0fd8b30e4b4d92ac56e65f2550f59bdb35413
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-01-07 16:49:13 +1100

    Added basic layout and colour styles
    Fixed header, nav and footer
    Added some colours

--- a2/index.php
+++ a2/index.php
@@ -18,13 +18,34 @@
     </header>
 
     <nav>
-      <div>Put website navigation links here</div>
+      <h1>Navigation</h1>
+      <ul>
+        <li>Home</li>
+        <li>Letter</li>
+        <li>Other</li>
+        <li>Filler</li>
+        <li>Pineapple</li>
+      </ul>
+
+      
     </nav>
 
     <main>
       <article id='Website Under Construction'>
     <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
         <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
+        <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
       </article>
     </main>
 
