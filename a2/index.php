e0a6f063610804d6e6945718e3badebbfa8d5f90
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-01-08 00:02:14 +1100

    Added watermark
    Added Poppy and rising sun to the footer

--- a2/index.php
+++ a2/index.php
@@ -47,14 +47,19 @@
         <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text</p>
         <p> Filler Text Filler TextFiller Tr TextFiller TextFiller TextFiller Text </p>
       </article>
+      
     </main>
 
     <footer>
+    <img src='../media/Poppysun.png' alt='Poppy & Rising Sun' class = 'watermark'/>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+        
+      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>. </div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+
     </footer>
 
   </body>
