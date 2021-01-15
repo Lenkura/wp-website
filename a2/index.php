126988ed36226bc74a3473c63223d32375d295ed
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-01-15 23:41:14 +1100

    New introduction page an content
    Set up the into page. Added the foreword to the home page

--- a2/index.php
+++ a2/index.php
@@ -14,9 +14,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans&display=swap" rel="stylesheet"> 
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -29,21 +29,19 @@
     <h1>Navigation</h1>
     <ul>
       <li id='location'>Home</li>
+      <a href="Introduction.html">
+        <li>Introduction</li>
+      </a>
       <a href="letters.html">
-        <li>Letter</li>
+        <li>Letters</li>
       </a>
       <a href=".html">
-        <li>Other</li>
+        <li>Links</li>
       </a>
       <a href=".html">
-        <li>Filler</li>
-      </a>
-      <a href=".html">
-        <li>Pineapple</li>
+        <li>Contact</li>
       </a>
     </ul>
-
-
   </nav>
 
   <main>
@@ -51,17 +49,26 @@
     <img src='../media/bakerdocs.png' alt='Service Record' id='service'>
     <article class='content'>
 
-      <h1> Welcome to the Douglas Raymond Baker Letter Collection
-      </h1>
+      <h2> Welcome to the Douglas Raymond Baker Letter Collection
+      </h2>
 
       <p>This year is the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period and inparticular, those who created the ANZAC legend. </p>
       <p>This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting his relatives in England during leave.
         They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.
         They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.
       </p>
-      <p>From the menu on the above, read the Introduction to set the scene. Then, to start reading the letters and post cards, click on Letters.</p>
+      <p>From the menu above, read the Introduction to set the scene. Then, to start reading the letters and post cards, click on Letters.</p>
     </article>
+    <<article>
 
+      <h2>Foreword by Ian Stuart Baker (Grandson of Douglas Raymond Baker)</h2>
+      <p>As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers.</p> <p>it would also help to recognise that in Grandfathers day, the letter was the only means of international
+      communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.</p> <p> From discussions
+      with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the
+      fears and concerns of those at home, inparticular, his mother and sisters.</p> <p> Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the
+      material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.
+</p>
+      </article>
 
   </main>
 
@@ -69,7 +76,7 @@
     <img src='../media/Poppy.png' alt='Poppy' class='watermark' /> <!-- image source : http://clipart-library.com/poppy-cliparts.html */ -->
     <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>. </div>
+      </script> Raymond Louey s3853718 Last modified <?= date("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>. </div>
     <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
 
     <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
