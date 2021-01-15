c04b804b8f4bf6a9ab6b7d4321c5e1ef3eca6a4c
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-01-16 00:15:31 +1100

    Transferred the content from the introduction page
    Moved and styled content from Introduction pages

--- a2/index.php
+++ a2/index.php
@@ -59,15 +59,18 @@
       </p>
       <p>From the menu above, read the Introduction to set the scene. Then, to start reading the letters and post cards, click on Letters.</p>
     </article>
-    <<article>
+    <article>
 
       <h2>Foreword by Ian Stuart Baker (Grandson of Douglas Raymond Baker)</h2>
-      <p>As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers.</p> <p>it would also help to recognise that in Grandfathers day, the letter was the only means of international
-      communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.</p> <p> From discussions
-      with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the
-      fears and concerns of those at home, inparticular, his mother and sisters.</p> <p> Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the
-      material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.
-</p>
+      <p>As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers.</p>
+      <p>it would also help to recognise that in Grandfathers day, the letter was the only means of international
+        communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.</p>
+      <p> From discussions
+        with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the
+        fears and concerns of those at home, inparticular, his mother and sisters.</p>
+      <p> Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the
+        material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.
+      </p>
       </article>
 
   </main>
