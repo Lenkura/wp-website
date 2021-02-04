c6a645e34b3b09f9004afd445c7adf82b3d87aaf
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-02-04 16:11:58 +1100

    Added Form Validation Checks
    Added server checks whih mimic the javascript ones. i.e check for any blanks and that name,mobile and email are the correct format

--- a3/contact.php
+++ a3/contact.php
@@ -8,7 +8,7 @@ start_module("Home");
         <article>
             <h1>Contact</h1>
             <p>If you would like to reach us for any reason, please fill out the following form</p>
-            <form action="https://titan.csit.rmit.edu.au/~e54061/wp/testcontact.php" method="POST" target='_blank' id='contactform'>
+            <form action="post-validation.php" method="POST" target='' id='contactform'>
                 <fieldset>
                     <legend>Contact Form</legend>
                     <label for="name">Name</label>
--- a3/post-validation.php
+++ a3/post-validation.php
@@ -4,7 +4,58 @@ $firstname = '';
 $firstnameError = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Put your POST data processing and validation code here
+$errorsfound = false;
+if (!isset($_POST["name"])) {
+  $nameError = "Name cannot be blank";
+  $errorsFound = true;
 }
 
+if(!preg_match("/^[-a-zA-Z ,.']+$/",$_POST["name"])){
+  $nameError = "Name contains unacceptable characters";
+  $errorsFound = true;
+}
+
+if (!isset($_POST["email"])) {
+  $nameError = "Email cannot be blank";
+  $errorsFound = true;
+}
+
+if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+  $nameError = "Incorrect email format";
+  $errorsFound = true;
+}
+
+if (!isset($_POST["mobile"])) {
+  $nameError = "Mobile cannot be blank";
+  $errorsFound = true;
+}
+if(!preg_match("/^(\(04\)|04|\+614)[ ]?\d{4}[ ]?\d{4}$/",$_POST["mobile"]){
+  $nameError = "Non Australian number entered";
+  $errorsFound = true;
+}
+
+if (!isset($_POST["subject"])) {
+  $nameError = "Subject cannot be blank";
+  $errorsFound = true;
+}
+
+if (!isset($_POST["message"])) {
+  $nameError = "Message cannot be blank";
+  $errorsFound = true;
+}
+
+
+
+
+if (!$errorsFound)
+  $message = "Here are your results ...";
+else
+  $message = "There are errors in your form";
+}
+
+$post = print_r($_POST, true);
+echo "<h3>\$_POST contains:</h3>
+<pre>
+  $post
+</pre>";
 ?>
