f60bd3e1bdf2f89e9884d50d0330c32d77bcbabb
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-02-04 19:07:25 +1100

    Php error now appear on the contact page
    Create a space for php errors to appear when entries don't meet criteria

--- a3/contact.php
+++ a3/contact.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang='en'>
 <?php require_once('tools.php');
+require_once('post-validation.php');
 start_module("Home");
 ?>
 
@@ -8,28 +9,28 @@ start_module("Home");
         <article>
             <h1>Contact</h1>
             <p>If you would like to reach us for any reason, please fill out the following form</p>
-            <form action="post-validation.php" method="POST" target='' id='contactform'>
+            <form action="contact.php" method="POST" target='' id='contactform'>
                 <fieldset>
                     <legend>Contact Form</legend>
                     <label for="name">Name</label>
                     <input type="text" id="name" name="name" placeholder="Douglas Raymond Baker" required onchange="nameerror()">
 
-                    <p class='contacterror' id='nameerror'><?=$nameError?></p>
+                    <p class='contacterror' id='nameerror'></p><?=$nameError?>
                     <label for="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Douglas@email.com" required onchange="emailerror()">
-                    <p class='contacterror' id='emailerror'></p>
+                    <input type="email" id="email" name="email" placeholder="Douglas@email.com" required onchange="emailerror()" value="<?=$cleanEmail?>">
+                    <p class='contacterror' id='emailerror'></p><?=$emailError?>
 
                     <label for="mobile">Mobile</label>
                     <input type="text" id="mobile" name="mobile" placeholder="0412345678" onchange="mobileerror()">
-                    <p class='contacterror' id='mobileerror'></p>
+                    <p class='contacterror' id='mobileerror'></p><?=$mobileError?>
 
                     <label for="subject">Subject</label>
                     <input type="text" id="subject" name="subject" placeholder="Reason for contact" required onchange="subjecterror()">
-                    <p class='contacterror' id='subjecterror'></p>
+                    <p class='contacterror' id='subjecterror'></p><?=$subjectError?>
 
                     <label for="message">Message</label>
                     <textarea id="message" name="message" placeholder="Enter your message" form="contactform" required onchange="messageerror()"></textarea>
-                    <p class='contacterror' id='messageerror'></p>
+                    <p class='contacterror' id='messageerror'></p><?=$messageError?>
 
                 </fieldset>
                 <input type='checkbox' id='remember' name='remember' value='remember' onclick="rememberme()"/>
--- a3/post-validation.php
+++ a3/post-validation.php
@@ -6,63 +6,66 @@ $emailError = '';
 $mobileError = '';
 $subjectError = '';
 $messageError = '';
-if (!empty($_POST)){
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $errorsfound = false;
-if (!isset($_POST["name"])) {
-  $nameError = "Name cannot be blank";
-  $errorsFound = true;
+
+if (!empty($_POST)) {
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (!isset($_POST["name"])) {
+      $nameError = '<span></span><span style="color:red; font-size:15px;">Name cannot be blank</span><span></span>';
+      $errorsFound = true;
+    }
+
+
+    if (!preg_match("/^[-a-zA-Z ,.']+$/", $_POST["name"])) {
+      $nameError = '<span></span><span style="color:red; font-size:15px;">Name contains unacceptable characters</span><span></span>';
+      $errorsFound = true;
+    }
+    
+    if (!isset($_POST["email"])) {
+      $emailError = '<span></span><span style="color:red; font-size:15px;">Email cannot be blank</span><span></span>';
+      $errorsFound = true;
+    } else
+  if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+      $emailError = '<span></span><span style="color:red; font-size:15px;">Incorrect email format</span><span></span>';
+      $errorsFound = true;
+      $cleanEmail = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+      //Present adjusted email to user
+    }
+
+    if (!isset($_POST["mobile"])) {
+      $mobileError = '<span></span><span style="color:red; font-size:15px;">Mobile cannot be blank</span><span></span>';
+      $errorsFound = true;
+    } else
+  if (!preg_match("/^(\(04\)|04|\+614)[ ]?\d{4}[ ]?\d{4}$/", $_POST["mobile"])) {
+      $mobileError = '<span></span><span style="color:red; font-size:15px;">Non Australian number entered</span><span></span>';
+      $errorsFound = true;
+    }
+
+    if (!isset($_POST["subject"])) {
+      $subjectError = '<span></span><span style="color:red; font-size:15px;">Subject cannot be blank</span><span></span>';
+      $errorsFound = true;
+    } else
+      $cleansubject = htmlentities($_POST["subject"]);
+
+    if (!isset($_POST["message"])) {
+      $messageError = '<span></span><span style="color:red; font-size:15px;">Message cannot be blank</span><span></span>';
+      $errorsFound = true;
+    } else
+      $cleanmessage = htmlentities($_POST["message"]);
+
+    //Present errors to user
+
+
+    if ($errorsFound) {
+      $message = "There are errors in your form, please try again";
+
+      // Present to user
+    }
+  }
 }
-
-if(!preg_match("/^[-a-zA-Z ,.']+$/",$_POST["name"])){
-  $nameError = "Name contains unacceptable characters";
-  $errorsFound = true;
-}
-
-if (!isset($_POST["email"])) {
-  $emailErrorB = "Email cannot be blank";
-  $errorsFound = true;
-} else
-if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
-  $emailError = "Incorrect email format";
-  $errorsFound = true;
-  $cleanEmail=filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-  //Present adjusted email to user
-}
-
-if (!isset($_POST["mobile"])) {
-  $mobileError = "Mobile cannot be blank";
-  $errorsFound = true;
-}else
-if(!preg_match("/^(\(04\)|04|\+614)[ ]?\d{4}[ ]?\d{4}$/",$_POST["mobile"]){
-  $mobileError = "Non Australian number entered";
-  $errorsFound = true;
-}
-
-if (!isset($_POST["subject"])) {
-  $subjectError = "Subject cannot be blank";
-  $errorsFound = true;
-} else
-$cleansubject = htmlentities($_POST["subject"]);
-
-if (!isset($_POST["message"])) {
-  $messageError = "Message cannot be blank";
-  $errorsFound = true;
-} else
-$cleanmessage = htmlentities($_POST["message"]);
-
-//Present errors to user
-
-
-if ($errorsFound){
-  $message = "There are errors in your form, please try again";
-
-  // Present to user
-}
-}
-$post = print_r($_POST, true);
-echo "<h3>\$_POST contains:</h3>
-<pre>
-  $post
-</pre>";
-?>
+// $post = print_r($_POST, true);
+// echo "<h3>\$_POST contains:</h3>
+// <pre>
+//   $post
+// </pre>";
